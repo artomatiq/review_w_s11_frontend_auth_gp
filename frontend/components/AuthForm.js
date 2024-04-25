affa9d56abd0e79b0c70d31368079b360975e89c
Art Vardanyan <akvardanyan99@gmail.com>
2024-04-25 15:00:35 -0400

    wip

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function AuthForm() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate()
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
@@ -11,13 +16,25 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
   const handleSubmit = async (event) => {
-
+    event.preventDefault()
+    setError('')
+    try {
+      const {data} = await axios.post('http://localhost:3003/api/auth/login', {
+        password: password,
+        username: username
+      })
+      localStorage.setItem('token', data.token)
+      navigate('/cereals')
+    }
+    catch (err) {
+      setError(err?.response?.data?.message || 'login failed')
+    }
   }
 
   return (
     <div className="container">
       <div aria-live="polite"></div>
-      <div aria-live="assertive" style={{ color: 'red' }}></div>
+      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
         <div>
--- frontend/components/Cereals.js
+++ frontend/components/Cereals.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Cereals() {
   const [cereals, setCereals] = useState([])
+  const navigate = useNavigate()
 
   const logout = () => {
-
+    localStorage.removeItem('token')
+    navigate('/')
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) navigate('/')
+    else {
+      axios.get(
+        'http://localhost:3003/api/cereals', 
+        { headers: {Authorization: token}}
+      )
+        .then (res => {
+          setCereals(res.data)
+        })
+        .catch (err => {
+          if (err?.response?.status == 401) logout()
+        })
+    }
+  }, [])
 
   return (
     <div className="container">
